79062116f123cd22a53ec06901772131b9c964aa
maisha-farihaa <maishafariha.32e@gmail.com>
2026-08-19 21:27:05 +0600

    events.js updated

--- routes/events.js
+++ routes/events.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+
+// get all events
+router.get('/', (req, res) => {
+  const events = db.prepare('SELECT * FROM events ORDER BY event_date ASC').all();
+  res.json(events);
+});
